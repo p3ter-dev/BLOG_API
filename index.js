07d6aff2e8b900d64208b08f698fb37110f4152f
peter <peterkinfe548@gmail.com>
2025-03-22 19:51:31 +0300

    modified the API endpoints

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 4000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// an example of a simple in-memory database
 let posts = [
     {
       id: 1,
@@ -34,6 +37,49 @@ let posts = [
   
 let lastId = 3;
 
+app.get('/posts', (req, res) => {
+    res.json(posts);
+})
+
+app.get('/posts/:id', (req, res) => {
+    const postId = req.params.id;
+    const post = posts.find(post => post.id == postId);
+    if (post) {
+        res.json(post);
+    } else {
+        res.status(404).json({ message: "sorry, post not found" });
+    }
+})
+
+app.post('/posts', (req, res) => {
+    const newPost = req.body;
+    newPost.id = ++lastId;
+    newPost.date = Date();
+    posts.push(newPost);
+    res.status(201).json(newPost);
+})
+
+app.patch('/posts/:id', (req, res) => {
+    const postId = req.params.id;
+    const post = posts.find(post => post.id == postId);
+
+    if (!post) return res.status(404).json({ message: "sorry, post not found"});
+    if (req.body.author) post.author = req.body.author;
+    if (req.body.title) post.title = req.body.title;
+    if (req.body.content) post.content = req.body.content;
+
+    res.json(post);
+})
+
+app.delete('/posts/:id', (req, res) => {
+  const postId = req.params.id;
+  const postIndex = posts.findIndex(post => post.id == postId);
+  if (postIndex == -1) return res.json({ message: "sorry, the post cannot be deleted" })
+  
+  posts.splice(postIndex, 1);
+
+  res.json({ message: "post deleted." });
+})
 
 app.listen(port, () => {
     console.log(`API is running on http://localhost:${port}`);
